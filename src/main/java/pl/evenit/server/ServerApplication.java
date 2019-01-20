ef326357a6f23ac55291024ed0065e77be14ed7a
mtszpater <mtszpater@gmail.com>
2019-01-20 13:50:53 +0100

    first commit

---
+++ src/main/java/pl/evenit/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package pl.evenit.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
+
